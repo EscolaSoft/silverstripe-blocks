58d9d216c4d9b9b1f4ee1d4c7a75642b0eb669a6
Magnum34 <kenko@onet.com.pl>
2017-08-22 22:30:33 +0200

    Add GridFieldGenerator

--- code/Block.php
+++ code/Block.php
@@ -8,173 +8,170 @@
  */
 class Block extends DataObject {
 
-    private static $singular_name = 'Block';
-  
-    private static $plural_name = 'Blocks';
-  
-    private static $first_write = false;
-  
-    protected $help = '';
+  private static $singular_name = 'Block';
+  private static $plural_name = 'Blocks';
+  private static $first_write = false;
+  protected $help = '';
 
-    const DIR_BLOCK = '/mysite/images/blocks/';
+  const DIR_BLOCK = '/mysite/images/blocks/';
 
-    private static $db = array(
-        'Active' => 'Boolean',
-        'SystemName' => 'Varchar',
+  private static $db = array(
+      'Active' => 'Boolean',
+      'SystemName' => 'Varchar',
+  );
+  private static $belongs_many_many = array(
+      'Pages' => 'BlocksPage'
+  );
+  private static $defaults = array(
+      'Active' => 1,
+      'Page_Blocks[SortOrder]' => 999, // TODO: Fix sorting, new blocks should be added to the bottom of the list/gridfield
+  );
+  private static $casting = array(
+      'createStringAsHTML' => 'HTMLText'
+  );
+  private static $summary_fields = array(
+      'ID' => 'ID',
+      'ClassName' => 'Type',
+      'SystemName' => 'SystemName',
+      'getIsActive' => 'Active',
+      'CMSThumbnail' => 'Preview',
+      'PageTitles' => 'Appears on'
+  );
+
+  public function getPageTitles() {
+    $titles = array();
+    foreach ($this->Pages() as $page) {
+      $titles[] = $page->Title;
+    }
+    return implode(', ', $titles);
+  }
+
+  /**
+   * Configuration style WYSIWYG (TinyMCE) for Title
+   * Title- > HTMLVarchar
+   *
+   */
+  public function __construct($record = null, $isSingleton = false, $model = null) {
+    parent::__construct($record, $isSingleton, $model);
+    $config = HtmlEditorConfig::get('reduced');
+    $config->disablePlugins('table');
+    $config->setButtonsForLine(2);
+    $config->setButtonsForLine(3);
+    $config->setButtonsForLine(1, 'bold');
+    $config->setOption('forced_root_block', '');
+    $config->setOption('force_br_newlines', true);
+    $config->setOption('force_p_newlines', false);
+    $config->setOption('invalid_elements', 'p');
+  }
+
+  public function createStringAsHTML($html) {
+    $casted = HTMLText::create();
+    $casted->setValue($html);
+
+    return $casted;
+  }
+
+  /**
+   * Add Inline Button for GirdField  more user friendly, so you can add many DataObject on one admin page without useless clicks.
+   * @param GridFieldConfig $conf =  GridFieldConfig_RelationEditor::create()
+   * @param string $title
+   * @return $conf
+   */
+  public function InlineButton($title = '') {
+    $conf = GridFieldConfig_RelationEditor::create(999);
+    $conf->removeComponentsByType('GridFieldAddNewButton');
+    $conf->addComponent(new Milkyway\SS\GridFieldUtils\AddNewInlineExtended($fragment = 'buttons-before-left', $title));
+    return $conf;
+  }
+
+  /**
+   *
+   * @param string $ButtonValue
+   * @param string $name
+   * @param string $title
+   * @param object $value
+   * @param boolean $orderable
+   * @return \GridField
+   */
+  public function GridFieldGenerator($ButtonValue, $name, $title, $value, $orderable = true) {
+    $conf = $this->InlineButton($ButtonValue);
+    $grid = new GridField(
+            $name, $title, $value, $conf
     );
-    private static $belongs_many_many = array(
-        'Pages' => 'BlocksPage'
-    );
-    private static $defaults = array(
-        'Active' => 1,
-        'Page_Blocks[SortOrder]' => 999, // TODO: Fix sorting, new blocks should be added to the bottom of the list/gridfield
-    );
-    private static $casting = array(
-        'createStringAsHTML' => 'HTMLText'
-    );
-    private static $summary_fields = array(
-        'ID' => 'ID',
-        'ClassName' => 'Type',
-        'SystemName' => 'SystemName',
-        'getIsActive' => 'Active',
-        'CMSThumbnail' => 'Preview',
-        'PageTitles' => 'Appears on'
-    );
-
-    public function getPageTitles() {
-        $titles = array();
-        foreach ($this->Pages() as $page) {
-            $titles[] = $page->Title;
-        }
-        return implode(', ', $titles);
+    if ($orderable) {
+      $grid->getConfig()->addComponent(new GridFieldOrderableRows());
     }
+    return $grid;
+  }
 
-    /**
-     * Configuration style WYSIWYG (TinyMCE) for Title
-     * Title- > HTMLVarchar
-     *
-     */
-    public function __construct($record = null, $isSingleton = false, $model = null) {
-        parent::__construct($record, $isSingleton, $model);
-        $config = HtmlEditorConfig::get('reduced');
-        $config->disablePlugins('table');
-        $config->setButtonsForLine(2);
-        $config->setButtonsForLine(3);
-        $config->setButtonsForLine(1, 'bold');
-        $config->setOption('forced_root_block', '');
-        $config->setOption('force_br_newlines', true);
-        $config->setOption('force_p_newlines', false);
-        $config->setOption('invalid_elements', 'p');
+  /**
+   * Path File Thumbnail
+   * @return path file
+   */
+  public static function CMSThumbnailPath() {
+    $filename = BASE_PATH . self::DIR_BLOCK . self::singleton()->class . '.png';
+    return $filename;
+  }
+
+  /**
+   *
+   * @return URL image
+   */
+  public static function CMSThumbnailURL() {
+    $filename = self::CMSThumbnailPath();
+
+    if (is_file($filename)) {
+      return BASE_URL . self::DIR_BLOCK . self::singleton()->class . '.png';
     }
+    return FALSE;
+  }
 
-    public function createStringAsHTML($html) {
-        $casted = HTMLText::create();
-        $casted->setValue($html);
+  public static function getCMSThumbnail() {
+    $url = self::CMSThumbnailURL();
+    $output = HTMLText::create();
+    $output->setValue('<img style="width:100px; height:auto;" src="' . $url . '" />');
+    return $output;
+  }
 
-        return $casted;
-    }
+  public static function getCMSHelp() {
+    return 'HELP please provide help!!!!';
+  }
 
-    /**
-     * Add Inline Button for GirdField  more user friendly, so you can add many DataObject on one admin page without useless clicks.
-     * @param GridFieldConfig $conf =  GridFieldConfig_RelationEditor::create()
-     * @param string $title
-     * @return $conf
-     */
-    public function InlineButton($title = '') {
-        $conf = GridFieldConfig_RelationEditor::create(999);
-        $conf->removeComponentsByType('GridFieldAddNewButton');
-        $conf->addComponent(new Milkyway\SS\GridFieldUtils\AddNewInlineExtended($fragment = 'buttons-before-left', $title));
-        return $conf;
-    }
+  public function CMSHelp() {
+    return self::getCMSHelp();
+  }
 
-    /**
-     * 
-     * @param string $ButtonValue
-     * @param string $name
-     * @param string $title
-     * @param object $value
-     * @param boolean $orderable
-     * @return \GridField
-     */
-    public function GridFieldGenerator($ButtonValue, $name, $title, $value, $orderable = true) {
-        $conf = $this->InlineButton($ButtonValue);
-        $grid = new GridField(
-                $name, $title, $value, $conf
-        );
-        if ($orderable) {
-            $grid->getConfig()->addComponent(new GridFieldOrderableRows());
-        }
-        return $grid;
-    }
+  public static function getCMSDescription() {
+    return 'HELP please provide description!!!!';
+  }
 
-    /**
-     * Path File Thumbnail
-     * @return path file
-     */
-    public static function CMSThumbnailPath() {
-        $filename = BASE_PATH . self::DIR_BLOCK . self::singleton()->class . '.png';
-        return $filename;
-    }
+  public function CMSDescription() {
+    return self::getCMSDescription();
+  }
 
-    /**
-     *
-     * @return URL image
-     */
-    public static function CMSThumbnailURL() {
-        $filename = self::CMSThumbnailPath();
+  /**
+   * image thumbnail
+   * @return HTML
+   */
+  public function CMSThumbnail() {
+    return self::getCMSThumbnail();
+  }
 
-        if (is_file($filename)) {
-            return BASE_URL . self::DIR_BLOCK . self::singleton()->class . '.png';
-        }
-        return FALSE;
-    }
+  public function getCMSFields() {
+    $fields = parent::getCMSFields();
+    $fields->removeByName('Pages');
 
-    public static function getCMSThumbnail() {
-        $url = self::CMSThumbnailURL();
-        $output = HTMLText::create();
-        $output->setValue('<img style="width:100px; height:auto;" src="' . $url . '" />');
-        return $output;
-    }
+    $help_content = '<p><strong>Description:</strong></p>';
+    $help_content .= $this->getCMSHelp();
+    $help_content .= '<hr/><p><strong>Instruction:</strong></p>';
+    $help_content .= $this->getCMSDescription();
+    $help_content .= '<hr/><p><strong>Thumbnail:</strong></p>';
+    $help_content .= '<img style="width:100%; max-width:600px; height:auto;" src="' . self::CMSThumbnailURL() . '" />';
 
-    public static function getCMSHelp() {
-        return 'HELP please provide help!!!!';
-    }
+    $fields->addFieldsToTab('Root.Help', new LiteralField('help', $help_content));
 
-    public function CMSHelp() {
-        return self::getCMSHelp();
-    }
-
-    public static function getCMSDescription() {
-        return 'HELP please provide description!!!!';
-    }
-
-    public function CMSDescription() {
-        return self::getCMSDescription();
-    }
-
-    /**
-     * image thumbnail
-     * @return HTML
-     */
-    public function CMSThumbnail() {
-        return self::getCMSThumbnail();
-    }
-
-    public function getCMSFields() {
-        $fields = parent::getCMSFields();
-        $fields->removeByName('Pages');
-
-        $help_content = '<p><strong>Description:</strong></p>';
-        $help_content .= $this->getCMSHelp();
-        $help_content .= '<hr/><p><strong>Instruction:</strong></p>';
-        $help_content .= $this->getCMSDescription();
-        $help_content .= '<hr/><p><strong>Thumbnail:</strong></p>';
-        $help_content .= '<img style="width:100%; max-width:600px; height:auto;" src="' . self::CMSThumbnailURL() . '" />';
-
-        $fields->addFieldsToTab('Root.Help', new LiteralField('help', $help_content));
-
-        if (isset($_GET['modal'])) {
-            Requirements::customCSS(<<<CSS
+    if (isset($_GET['modal'])) {
+      Requirements::customCSS(<<<CSS
             #cms-menu,
             #Form_ItemEditForm .cms-content-header.north,
             #Form_ItemEditForm .better-buttons-utils,
@@ -191,72 +188,72 @@ class Block extends DataObject {
             }
 CSS
       );
-        }
-        return $fields;
     }
+    return $fields;
+  }
 
-    public function canView($member = null) {
-        return Permission::check('ADMIN') || Permission::check('CMS_ACCESS_BlockAdmin') || Permission::check('CMS_ACCESS_LeftAndMain');
+  public function canView($member = null) {
+    return Permission::check('ADMIN') || Permission::check('CMS_ACCESS_BlockAdmin') || Permission::check('CMS_ACCESS_LeftAndMain');
+  }
+
+  public function canEdit($member = null) {
+    return Permission::check('ADMIN') || Permission::check('CMS_ACCESS_BlockAdmin') || Permission::check('CMS_ACCESS_LeftAndMain');
+  }
+
+  public function canCreate($member = null) {
+    return Permission::check('ADMIN') || Permission::check('CMS_ACCESS_BlockAdmin') || Permission::check('CMS_ACCESS_LeftAndMain');
+  }
+
+  public function canPublish($member = null) {
+    return Permission::check('ADMIN') || Permission::check('CMS_ACCESS_BlockAdmin') || Permission::check('CMS_ACCESS_LeftAndMain');
+  }
+
+  private static $searchable_fields = array(
+      'ID' => 'PartialMatchFilter',
+      'SystemName' => 'PartialMatchFilter',
+      'Active'
+  );
+
+  public function populateDefaults() {
+    $this->SystemName = $this->getClassName();
+    parent::populateDefaults();
+  }
+
+  public function getIsActive() {
+    return $this->Active ? 'Yes' : 'No';
+  }
+
+  protected function onBeforeWrite() {
+    if ($this->SystemName == '') {
+      $this->SystemName = $this->getClassName();
     }
+    parent::onBeforeWrite();
+  }
 
-    public function canEdit($member = null) {
-        return Permission::check('ADMIN') || Permission::check('CMS_ACCESS_BlockAdmin') || Permission::check('CMS_ACCESS_LeftAndMain');
-    }
+  function forTemplate() {
 
-    public function canCreate($member = null) {
-        return Permission::check('ADMIN') || Permission::check('CMS_ACCESS_BlockAdmin') || Permission::check('CMS_ACCESS_LeftAndMain');
-    }
+    // can we include the Parent page for rendering? Perhaps use a checkbox in the CMS on the block if we should include the Page data.
+    // $page = Controller::curr();
+    // return $this->customise(array('Page' => $page))->renderwith($this->Template);
+    return $this->renderWith(array($this->class, 'Block')); // Fall back to Block if selected does not exist
+  }
 
-    public function canPublish($member = null) {
-        return Permission::check('ADMIN') || Permission::check('CMS_ACCESS_BlockAdmin') || Permission::check('CMS_ACCESS_LeftAndMain');
-    }
+  /**
+   * Returns the page object (SiteTree) that we are currently on
+   * Allow us to loop on children of the page and other page related data
+   *
+   * @return SiteTree
+   */
+  public function CurrentPage() {
+    return Director::get_current_page();
+  }
 
-    private static $searchable_fields = array(
-        'ID' => 'PartialMatchFilter',
-        'SystemName' => 'PartialMatchFilter',
-        'Active'
-    );
+  public function CurrentController() {
+    return Controller::curr();
+  }
 
-    public function populateDefaults() {
-        $this->SystemName = $this->getClassName();
-        parent::populateDefaults();
-    }
-
-    public function getIsActive() {
-        return $this->Active ? 'Yes' : 'No';
-    }
-
-    protected function onBeforeWrite() {
-        if ($this->SystemName == '') {
-            $this->SystemName = $this->getClassName();
-        }
-        parent::onBeforeWrite();
-    }
-
-    function forTemplate() {
-
-        // can we include the Parent page for rendering? Perhaps use a checkbox in the CMS on the block if we should include the Page data.
-        // $page = Controller::curr();
-        // return $this->customise(array('Page' => $page))->renderwith($this->Template);
-        return $this->renderWith(array($this->class, 'Block')); // Fall back to Block if selected does not exist
-    }
-
-    /**
-     * Returns the page object (SiteTree) that we are currently on
-     * Allow us to loop on children of the page and other page related data
-     *
-     * @return SiteTree
-     */
-    public function CurrentPage() {
-        return Director::get_current_page();
-    }
-
-    public function CurrentController() {
-        return Controller::curr();
-    }
-
-    public function getEditLink() {
-        return Director::baseURL() . 'admin/pages/edit/EditForm/field/Blocks/item/' . $this->ID . '/edit?modal=1';
-    }
+  public function getEditLink() {
+    return Director::baseURL() . 'admin/pages/edit/EditForm/field/Blocks/item/' . $this->ID . '/edit?modal=1';
+  }
 
 }
